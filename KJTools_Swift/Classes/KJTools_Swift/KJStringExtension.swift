3a01719fce27d33003b090502d5d2e8f9f9e90bc
蒲炜东 <puweidongyx@gmail.com>
2019-05-29 23:12:40 +0800

    修改部分内容

--- KJTools_Swift/Classes/KJTools_Swift/KJStringExtension.swift
+++ KJTools_Swift/Classes/KJTools_Swift/KJStringExtension.swift
@@ -12,23 +12,23 @@ import UIKit
 // 下标截取任意位置的便捷方法
 extension String {
     
-    var length: Int {
+    public var length: Int {
         return self.count
     }
     
-    subscript (i: Int) -> String {
+    public subscript (i: Int) -> String {
         return self[i ..< i + 1]
     }
     
-    func substring(fromIndex: Int) -> String {
+    public func substring(fromIndex: Int) -> String {
         return self[min(fromIndex, length) ..< length]
     }
     
-    func substring(toIndex: Int) -> String {
+    public func substring(toIndex: Int) -> String {
         return self[0 ..< max(0, toIndex)]
     }
     
-    subscript (r: Range<Int>) -> String {
+    public subscript (r: Range<Int>) -> String {
         let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)), upper: min(length, max(0, r.upperBound))))
         let start = index(startIndex, offsetBy: range.lowerBound)
         let end = index(start, offsetBy: range.upperBound - range.lowerBound)
@@ -36,12 +36,110 @@ extension String {
     }
     
 }
-//不包含后几个字符串的方法
+
+
+//MARK: - 获取文本高度(宽度)
 extension String {
-    func dropLast(_ n: Int = 1) -> String {
-        return String(characters.dropLast(n))
+    
+    
+    /// 获取字符串尺寸--私有方法
+    ///
+    /// - Parameters:
+    ///   - str: 字符串
+    ///   - attriStr: 富文本
+    ///   - font: 字体大小
+    ///   - w: 宽度
+    ///   - h: 高度
+    /// - Returns: 宽、高
+    private func getNormalStrSize(str: String? = nil, attriStr: NSMutableAttributedString? = nil, font: CGFloat, w: CGFloat, h: CGFloat) -> CGSize {
+        if str != nil {
+            let strSize = (str! as NSString).boundingRect(with: CGSize(width: w, height: h), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: font)], context: nil).size
+            return strSize
+        }
+        
+        if attriStr != nil {
+            let strSize = attriStr!.boundingRect(with: CGSize(width: w, height: h), options: .usesLineFragmentOrigin, context: nil).size
+            return strSize
+        }
+        return CGSize.zero
     }
-    var dropLast: String {
-        return dropLast()
+    
+    
+    /// 获取富文本字符串高度
+    ///
+    /// - Parameters:
+    ///   - attriStr: 富文本
+    ///   - strFont: 字体大小
+    ///   - w: 宽度
+    /// - Returns: 高度
+    public func getAttributedStrH(attriStr: NSMutableAttributedString, strFont: CGFloat, w: CGFloat) -> CGFloat {
+        return getNormalStrSize(attriStr: attriStr, font: strFont, w: w, h: CGFloat.greatestFiniteMagnitude).height
     }
+    
+    
+    
+    /// 获取富文本字符串宽度W
+    ///
+    /// - Parameters:
+    ///   - attriStr: 富文本
+    ///   - strFont: 字体大小
+    ///   - h: 高度
+    /// - Returns: 宽度
+    public func getAttributedStrW(attriStr: NSMutableAttributedString, strFont: CGFloat, h: CGFloat) -> CGFloat {
+        return getNormalStrSize(attriStr: attriStr, font: strFont, w: CGFloat.greatestFiniteMagnitude, h: h).width
+    }
+    
+    
+    /// 获取字符串高度H
+    ///
+    /// - Parameters:
+    ///   - str: 字符串
+    ///   - strFont: 字体大小
+    ///   - w: 宽度
+    /// - Returns: 高度
+    public func getNormalStrH(str: String, strFont: CGFloat, w: CGFloat) -> CGFloat {
+        return getNormalStrSize(str: str, font: strFont, w: w, h: CGFloat.greatestFiniteMagnitude).height
+    }
+    
+    
+    /// 获取字符串宽度W
+    ///
+    /// - Parameters:
+    ///   - str: 字符串
+    ///   - strFont: 字体大小
+    ///   - h: 高度
+    /// - Returns: 宽度
+    public func getNormalStrW(str: String, strFont: CGFloat, h: CGFloat) -> CGFloat {
+        return getNormalStrSize(str: str, font: strFont, w: CGFloat.greatestFiniteMagnitude, h: h).width
+    }
+    
+    
+    
+//    /// 获取文本高度
+//    ///
+//    /// - Parameters:
+//    ///   - font: 字体大小
+//    ///   - maxWidth: 最大宽度
+//    public func getTextHeight(WithFont font: UIFont = UIFont.systemFont(ofSize: 15), maxWidth: CGFloat) -> CGFloat {
+//        let normalText: String = self
+//        let size = CGSize.init(width: maxWidth, height: 1000)
+//        let dic = NSDictionary(object: font, forKey: NSAttributedString.Key.font as NSCopying)
+//        let stringSize = normalText.boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: dic as? [NSAttributedString.Key : Any], context:nil).size
+//        return stringSize.height
+//    }
+
+    
+    /// 获取文本宽度
+    ///
+    /// - Parameters:
+    ///   - font: 字体大小
+    ///   - maxHeight: 最大高度
+//    public func getTextWidth(WithFont font: UIFont = UIFont.systemFont(ofSize: 15), maxHeight: CGFloat) -> CGFloat {
+//        let normalText: String = self
+//        let size = CGSize(width: 1000, height: maxHeight)
+//        let dic = NSDictionary(object: font, forKey: NSAttributedString.Key.font as NSCopying)
+//        let stringSize = normalText.boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: dic as? [NSAttributedString.Key : Any], context:nil).size
+//        return stringSize.width
+//    }
+    
 }
